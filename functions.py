0c0932d4ac23c44990f3fb6e9087728dafc88056
Pedro F Ormenese <pedro.f.ormenese@gmail.com>
2025-08-26 08:12:08 -0300

    Update functions.py
    Adicionamos a função do menu inicial

--- functions.py
+++ functions.py
@@ -1,3 +1,12 @@
-import os
+def menu():
+    menu_options = ["Acessar biblioteca", "Conferir livros alugados", "Sair"]
 
-def menu(livros):
+    while True:
+        print("=== Bem-vindo à biblioteca ===")
+        number = 1
+
+        for opcao in menu_options:
+            print(f"{number}. {opcao}\n")
+            number += 1
+        menu_question = int(input("O que você gostaria de fazer?\n\n--> "))
+      
