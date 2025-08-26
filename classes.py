eef02e82568e3c38b86ca3d6b9a31021edbb1306
Guilherme <guilhermecassimiro425@gmail.com>
2025-08-26 07:56:31 -0300

    Função menu criada

--- classes.py
+++ classes.py
@@ -1,2 +1,8 @@
 class Livro:
-    def__init
+    def __init__(self, título, autor, gênero, ano, editora,):
+        self.__título = título
+        self.__autor = autor
+        self.__gênero = gênero
+        self.__ano = ano
+        self.__editora = editora
+
