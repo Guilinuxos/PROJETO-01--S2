424496ebc53c1353b91fd7b9f1301d73686a7f95
hillary <hillarycarvalho2510@gmail.com>
2025-08-26 08:43:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,49 +3,29 @@ lista de livros
 gêneros - filosofia, romance, história, contos, ação, terror, infanto juvenil, poesia
 
 
-livro1 = Livro("Dom Casmurro, "Machado de Assis", "Romance")
+livro1 = Livro("Dom Casmurro", "Machado de Assis", "Romance")
+livro2 = Livro("Memórias Póstumas de Brás Cubas", "Machado de Assis", "Romance")
+livro3 = Livro("Quincas Borba", "Machado de Assis", "Romance")
+livro4 = Livro("Drácula", "Frankenstein", "Terror")
+livro5 = Livro("Drácula", "Bram Stoker", "Terror")
+livro6 = Livro("Evangelho de Sangue", "Clive Barker", "Terror")
+livro7 = Livro("Morangos Mofados", "Caio Fernando Abreu", "Contos")
+livro8 = Livro("O Alienista", "Machado de Assis", "Contos")
+livro9 = Livro("O Cartomante", "Machado de Assis", "Contos")
+livro10 = Livro("Marília de Dirceu", "Tomás António Gonzaga", "Poesia")
+livro11 = Livro("Toda poesia", "Paulo Leminski", "Poesia")
+livro12 = Livro("Os Lusíadas", "Luís de Camões", "Poesia")
+livro13 = Livro("Alguma Poesia", "Carlos Drummond de Andrade", "Poesia")
+livro14 = Livro("Star Wars: Marcas da Guerra", "Chuck Wendig", "Ação")
+livro15 = Livro("A Bússola de Ouro", "Philip Pullman", "Ação")
+livro16 = Livro("Ponto de Impacto", "Dan Brown", "Ação")
+livro17 = Livro("Apologia de Sócrates", "Platão", "Filosofia")
+livro18 = Livro("O príncipe", "Maquiavel", "Filosofia")
+livro19 = Livro("A república", "Platão", "Filosofia")
+livro21 = Livro("Retórica", "Aristóteles", "Filosofia")
+livro22 = Livro("O Diário de Anne Frank", "Anne Frank", "História")
+livro23 = Livro("Sapiens: Uma Breve História da Humanidade", "Yuval Noah Harari", "História")
+livro24 = Livro("Diário de um banana", "Jeff Kinney", "Infanto Juvenil")
+livro25 = Livro("As Crônicas de Nárnia", "C. S. Lewis", "Infanto Juvenil")
 
-livro2 = Livro(”Memórias Póstumas de Brás Cubas”, “Machado de Assis”, “Romance”)
 
-livro3 = Livro(”Quincas Borba”, “Machado de Assis”, “Romance”)
-
-livro4 = Livro(”Drácula”, “Frankenstein”, “Terror”)
-
-livro5 = Livro(”Drácula”, “Bram Stoker”, Terror”)
-
-livro6 = “Evangelho de Sangue”, “Clive Barker”, “Terror”)
-
-livro7 = Livro("Morangos Mofados", "Caio Fernando Abreu", “Contos”)
-
-livro8 = Livro("O Alienista", "Machado de Assis", “Contos”)
-
-livro9 = Livro("O Cartomante", "Machado de Assis", “Contos”)
-
-livro10 = Livro ("Marília de Dirceu", “Tomás António Gonzaga”, “poesia”)
-
-livro11 = Livro ("Toda poesia", Paulo Leminski", “Poesia")
-
-livro12 = Livro ("Os Lusíadas", "Luís de Camões", “Poesia”)
-
-livro13 = Livro("Alguma Poesia", "Carlos Drummond de Andrade", “Poesia”)
-
-livro14 = Livro (”Star Wars: Marcas da Guerra”, “Chuck Wendig”, “Ação”)
-
-livro15 = Livro (”A Bússola de Ouro”, Philip Pullman”, “Ação”)
-
-livro16 = Livro (”Ponto de Impacto”, “ Dan Brown”, “Ação”)
-
-livro17 = Livro (”Apologia de Sócrates”, “Platão”, “Filosofia”)
-
-livro18 = Livro (”O príncipe”, “Maquiavel”, “Filosofia”)
-
-livro19 = Livro (”A república”, “Platão”, “Filosofia”)
-
-livro21 = Livro (”Retórica”, “Aristóteles”, “Filosofia”)
-
-livro22 = Livro (”O Diário de Anne Frank”, “Anne Frank”, “História”)
-
-livro23 = Livro (”Sapiens: Uma Breve **História** da Humanidade”, “Yuval Noah Harari”, “História”)
-livro24 = Livro (”Diário de um banana”, “Jeff Kinney”, “Infanto Juvenil”)
-
-livro25 = Livro ("As Crônicas de Nárnia", "C. S. Lewis.", “Infanto Juvenil")
