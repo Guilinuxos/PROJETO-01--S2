30ca5ba0eccc8ba0a0f3c363c6e609f7663a737d
Pedro F Ormenese <pedro.f.ormenese@gmail.com>
2025-08-26 09:03:59 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,20 @@
+# ==================== Sair
+
+
+
+
+# ==================== Conferir livros alugados
+
+
+
+
+# ==================== Acessar biblioteca
+
+
+
+
+# ==================== Menu Principal
+
 def menu():
     menu_options = ["Acessar biblioteca", "Conferir livros alugados", "Sair"]
 
@@ -9,4 +26,14 @@ def menu():
             print(f"{number}. {opcao}\n")
             number += 1
         menu_question = int(input("O que você gostaria de fazer?\n\n--> "))
-      
+
+        if menu_question > 3 or menu_question < 1:
+            while menu_question > 3 or menu_question < 1:
+                print("Escolha uma opção disponível")
+                menu_question = int(input("--> "))
+        elif menu_question == 1:
+            pass
+        elif menu_question == 2:
+            pass
+        elif menu_question == 3:
+            pass
