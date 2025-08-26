5a81e21383ff01207ba94a307edd9507d0839b68
Pedro F Ormenese <pedro.f.ormenese@gmail.com>
2025-08-26 08:17:30 -0300

    Update classes.py
    Adicionei um atributo de disponibilidade para o livro

--- classes.py
+++ classes.py
@@ -1,8 +1,9 @@
 class Livro:
-    def __init__(self, título, autor, gênero, ano, editora,):
-        self.__título = título
+    def __init__(self, titulo, autor, gênero, ano, editora,):
+        self.__título = titulo
         self.__autor = autor
         self.__gênero = gênero
         self.__ano = ano
         self.__editora = editora
+        self.__disponivel = True
 
